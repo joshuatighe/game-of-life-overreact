293745fd022cb3704446fc1e9b25ae7dd4402a03
Joshua Tighe <jtighe3@nd.edu>
2025-07-14 18:02:23 +0100

    feat: add tailwind and some placeholder

--- src/GameOfLife.tsx
+++ src/GameOfLife.tsx
@@ -1,7 +1,10 @@
 export const GameOfLife = () => {
   return (
-    <>
-      game of life
-    </>
+    <div className="min-h-screen bg-gray-800 flex flex-col justify-around items-center">
+      <h1 className="text-neutral-50 font-bold text-3xl">game of life!</h1>
+      <div className="min-w-[500px] min-h-[500px] bg-gray-300">
+
+      </div>
+    </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GameOfLife } from './GameOfLife.tsx'
 import { Engine } from '@overreact/engine'
+import './Main.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
